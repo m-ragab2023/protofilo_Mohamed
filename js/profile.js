04d04961702010ef40d09f78c1bd405b3dd23174
mohamed <mohamedalsaifi00@gmail.com>
2024-11-18 18:12:48 +0200

    My protfilo

---
+++ js/profile.js
@@ -0,0 +1,12 @@
+let action = document.getElementById("action");
+let menu = document.getElementById("menu");
+
+
+
+menu.addEventListener("click", ()=>{
+   action.classList.toggle('is-active');
+});
+ 
+
+
+
