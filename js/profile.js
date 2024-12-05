dc1bb892d394bbeb2addef221b96b1c5b7a44f5a
mohamed <mohamedalsaifi00@gmail.com>
2024-12-05 15:40:06 +0200

    this is three commit

--- js/profile.js
+++ js/profile.js
@@ -1,12 +1,28 @@
 let action = document.getElementById("action");
 let menu = document.getElementById("menu");
 
-
-
-menu.addEventListener("click", ()=>{
-   action.classList.toggle('is-active');
+menu.addEventListener("click", () => {
+  action.classList.toggle("is-active");
 });
- 
+
+let btn_scroll = document.querySelector(".btn-scroll");
+console.log(btn_scroll);
+
+window.onscroll = function () {
+  if (scrollY >= 500) {
+    btn_scroll.style.display = "block";
+  } else {
+    btn_scroll.style.display = "none";
+  }
+};
+
+btn_scroll.onclick = function () {
+  scroll({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
 
 
 
